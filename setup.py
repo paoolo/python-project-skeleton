e59bbe3abd6d99c381647951e5917bb0c03a1c92
Pawel Suder <pasuder@cisco.com>
2021-01-13 10:36:10 +0100

    Bump to version 1.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     test_suite="python_project_skeleton.tests",
     include_package_data=True,
     install_requires=required,
-    version='1.0',
+    version='1.1',
     description='python project skeleton',
     author=u'Paweł Suder',
     author_email='pawel@suder.info',
